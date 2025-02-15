edfadf8f0f9a22682d011446e5b8663c10dabaa5
Lucian Chen <lca193@sfu.ca>
2025-02-15 10:49:42 -0800

    Added collapsible shield for classes

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 var coll = document.getElementsByClassName("collapsible");
 var i;
-
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
+    
+    // Toggle max-height
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    } 
+      content.style.maxHeight = content.scrollHeight + "vw";
+      
+      // Scroll content into view
+      content.scrollIntoView({ behavior: 'smooth', block: 'start' });
+    }
   });
 }
